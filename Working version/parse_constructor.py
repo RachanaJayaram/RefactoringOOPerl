4c21c1d7053fb7650c8d1d921af9677f12f66e66
skandavc18 <skandavc18@gmail.com>
2019-04-20 23:20:03 +0530

    made many changes. independent support for hash.one statement can be removed. See comments.

--- Working version/parse_constructor.py
+++ Working version/parse_constructor.py
@@ -4,24 +4,30 @@ def p_constructor(p):
     '''constructor : SUB NEW BRACES_LEFT constructor_body BRACES_RIGHT'''
     output = "\n" + "def __init__(self,*argv) : \n"
     output += "\t" + "arg_list = list(argv)[::-1]\n"
-    output += "\t" + "self.__dict__.update({"
-    p[0] = output + p[4] + '})'
+    output += "\t" + "self.__dict__.update("
+    p[0] = output + p[4] + ')'
 
 def p_constructor_body(p):
     '''constructor_body : statement constructor_hash bless_st return_constructor'''
     p[0] = p[2]
 
 def p_constructor_hash(p):
-    '''constructor_hash : my ASSIGNOP BRACES_LEFT hash_statement_list BRACES_RIGHT SEMI'''
-    p[0] = p[4]
+    '''constructor_hash : my NAME ASSIGNOP hash_statement SEMI'''  # can remove it and add it to var_dec itself. Try it
+    p[0] = p[5]
 
-def p_hash_statement_list(p):  
-    '''hash_statement_list : hash_statement
-                           | hash_statement_list COMMA hash_statement'''
-    p[0] = ''.join(p[1:]) 
 
 def p_hash_statement(p):
-    '''hash_statement : NAME HASH_OP SHIFT
+    '''hash_statement : BRACES_LEFT hash_statement_list BRACES_RIGHT'''
+    p[0] = '{ '+p[2]+' }'
+
+
+def p_hash_statement_list(p):  
+    '''hash_statement_list : hash_statement_part
+                           | hash_statement_list COMMA hash_statement_part'''
+    p[0] = ''.join(p[1:]) 
+
+def p_hash_statement_part(p):
+    '''hash_statement_part : NAME HASH_OP SHIFT
                       | NAME HASH_OP term'''
     if p[3] == "shift":
         p[0] = "'" + p[1] + "':arg_list.pop()"
